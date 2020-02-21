8cee259ed7a42390222ccbee70c71001428ddcd7
sunilt4 <sunil.tumkur@uoit.net>
2020-02-21 18:50:00 -0500

    Update myshell.h

--- myshell.h
+++ myshell.h
@@ -1,13 +1,5 @@
 //myshell header file 
 
-/*
- * MyShell Project for SOFE 3950U: Operating Systems Lab 2
- *
- * Copyright (C) 2020, <Sunil Tumkur, Matthew Brown, Eric Tsim, >
- * All rights reserved.
- * 
- */
-
 /* 
 checks whether the given token has been #defined earlier in the file. 
 if not, it includes the code between it and the closing #endif statement
