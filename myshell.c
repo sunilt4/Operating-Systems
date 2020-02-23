66d0f3b99313d20e8f8e975655cbe0fe14e46110
sunilt4 <sunil.tumkur@uoit.net>
2020-02-22 23:42:43 -0500

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -34,19 +34,36 @@ int int main(int argc, char *argv[], char **envp)
 
 	//getcwd() function shall place an absolute pathname of the current working directory in the array pointed to by cwd, and return cwd
 	if (getcwd(cwd, sizeof(cwd)) != NULL) {
-		
 		//print current working directory
 		printf("%s ", cwd);
 	}
 
 	else {
-
 		//print out an error if you cant find and display current working directory 
 		perror("getcwd error!")
-		return -1;
+		return 1;
 	}
 
 	//Parse commands given using argc ad argv and perfirn an infinite loop getting command input from users
+
+	while (fegts(buffer, BUFFER_LEN, stdin) != NULL {
+
+		// Perform string tokenization to get the command and argument
+		token = strtok(buffer, s);
+		int counter = 0;
+
+		while(token != NULL) {
+
+			if (counter == 0) {
+
+				memcpy(command, token, strlen(token));
+			}
+
+			arr[counter] = token;
+			token = strtok(NULL, s);
+			counter++;
+		}
+	}
 
 
 
