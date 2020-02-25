566316203a99b07239082a27d3f138825d44998b
sunilt4 <sunil.tumkur@uoit.net>
2020-02-24 20:01:05 -0500

    Update utility.h

---
+++ utility.h
@@ -0,0 +1,8 @@
+#ifndef UTILITY_H_
+#define UTILITY_H_
+
+// Include your relevant functions declarations here they must start with the
+// extern keyword such as in the following example:
+// extern void display_help(void);
+
+#endif
